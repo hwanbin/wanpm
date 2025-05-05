41584914b7ccf232a7522750944ebd0cdc0745fb
hwanbin <55409389+hwanbin@users.noreply.github.com>
2025-05-05 00:19:13 -0400

    group routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -36,57 +36,59 @@ func (app *application) routes() http.Handler {
 	router.Put("/v1/user/activate", app.activateUserHandler)
 	router.Post("/v1/user/authenticate", app.authenticateUserHandler)
 
-	if app.config.env == "remote-dev" {
-		router.Use(app.authRequired)
-	}
+	router.Group(func(mux chi.Router) {
+		if app.config.env == "remote-dev" {
+			mux.Use(app.authRequired)
+		}
 
-	router.Get("/v1/geocode/forward", app.forwardGeocodeHandler)
+		mux.Get("/v1/geocode/forward", app.forwardGeocodeHandler)
 
-	router.Get("/v1/project", app.listProjectHandler)
-	router.Post("/v1/project", app.createProjectHandler)
-	router.Get("/v1/project/{id}", app.showProjectHandler)
-	router.Patch("/v1/project/{id}", app.updateProjectHandler)
-	router.Delete("/v1/project/{id}", app.deleteProjectHandler)
+		mux.Get("/v1/project", app.listProjectHandler)
+		mux.Post("/v1/project", app.createProjectHandler)
+		mux.Get("/v1/project/{id}", app.showProjectHandler)
+		mux.Patch("/v1/project/{id}", app.updateProjectHandler)
+		mux.Delete("/v1/project/{id}", app.deleteProjectHandler)
 
-	router.Get("/v1/client", app.listClientHandler)
-	router.Post("/v1/client", app.createClientHandler)
-	router.Get("/v1/client/{id}", app.showClientHandler)
-	router.Patch("/v1/client/{id}", app.updateClientHandler)
-	router.Delete("/v1/client/{id}", app.deleteClientHandler)
+		mux.Get("/v1/client", app.listClientHandler)
+		mux.Post("/v1/client", app.createClientHandler)
+		mux.Get("/v1/client/{id}", app.showClientHandler)
+		mux.Patch("/v1/client/{id}", app.updateClientHandler)
+		mux.Delete("/v1/client/{id}", app.deleteClientHandler)
 
-	router.Post("/v1/proposal", app.createProposalHandler)
-	router.Get("/v1/proposal/{id}", app.showProposalHandler)
-	router.Patch("/v1/proposal/{id}", app.updateProposalHandler)
-	router.Delete("/v1/proposal/{id}", app.deleteProposalHandler)
+		mux.Post("/v1/proposal", app.createProposalHandler)
+		mux.Get("/v1/proposal/{id}", app.showProposalHandler)
+		mux.Patch("/v1/proposal/{id}", app.updateProposalHandler)
+		mux.Delete("/v1/proposal/{id}", app.deleteProposalHandler)
 
-	router.Get("/v1/presigned-put", app.createPresignedPutUrlHandler)
-	router.Get("/v1/presigned-get", app.createPresignedGetUrlHandler)
-	router.Get("/v1/presigned-delete", app.createPresignedDeleteUrlHandler)
+		mux.Get("/v1/presigned-put", app.createPresignedPutUrlHandler)
+		mux.Get("/v1/presigned-get", app.createPresignedGetUrlHandler)
+		mux.Get("/v1/presigned-delete", app.createPresignedDeleteUrlHandler)
 
-	router.Get("/v1/list-files", app.listFilesWithPrefixHandler)
+		mux.Get("/v1/list-files", app.listFilesWithPrefixHandler)
 
-	router.Post("/v1/activity", app.createActivityHandler)
-	router.Get("/v1/activity/{id}", app.showActivityHandler)
-	router.Get("/v1/activity", app.listActivityHandler)
-	router.Patch("/v1/activity/{id}", app.updateActivityHandler)
-	router.Delete("/v1/activity/{id}", app.deleteActivityHandler)
+		mux.Post("/v1/activity", app.createActivityHandler)
+		mux.Get("/v1/activity/{id}", app.showActivityHandler)
+		mux.Get("/v1/activity", app.listActivityHandler)
+		mux.Patch("/v1/activity/{id}", app.updateActivityHandler)
+		mux.Delete("/v1/activity/{id}", app.deleteActivityHandler)
 
-	router.Post("/v1/role", app.createRoleHandler)
-	router.Get("/v1/role/{id}", app.showRoleHandler)
-	router.Get("/v1/role", app.listRoleHandler)
-	router.Patch("/v1/role/{id}", app.updateRoleHandler)
-	router.Delete("/v1/role/{id}", app.deleteRoleHandler)
+		mux.Post("/v1/role", app.createRoleHandler)
+		mux.Get("/v1/role/{id}", app.showRoleHandler)
+		mux.Get("/v1/role", app.listRoleHandler)
+		mux.Patch("/v1/role/{id}", app.updateRoleHandler)
+		mux.Delete("/v1/role/{id}", app.deleteRoleHandler)
 
-	router.Get("/v1/user/refresh", app.refreshTokenHandler)
-	router.Get("/v1/user/logout", app.logoutHandler)
-	router.Get("/v1/user/{id}", app.getUserHandler)
-	router.Get("/v1/user", app.listUsersHandler)
+		mux.Get("/v1/user/refresh", app.refreshTokenHandler)
+		mux.Get("/v1/user/logout", app.logoutHandler)
+		mux.Get("/v1/user/{id}", app.getUserHandler)
+		mux.Get("/v1/user", app.listUsersHandler)
 
-	router.Post("/v1/timesheet", app.createTimesheetHandler)
-	router.Get("/v1/timesheet/{id}", app.showTimesheetHandler)
-	router.Get("/v1/timesheet", app.listTimesheetHandler)
-	router.Patch("/v1/timesheet/{id}", app.updateTimesheetHandler)
-	router.Delete("/v1/timesheet/{id}", app.deleteTimesheetHandler)
-	
+		mux.Post("/v1/timesheet", app.createTimesheetHandler)
+		mux.Get("/v1/timesheet/{id}", app.showTimesheetHandler)
+		mux.Get("/v1/timesheet", app.listTimesheetHandler)
+		mux.Patch("/v1/timesheet/{id}", app.updateTimesheetHandler)
+		mux.Delete("/v1/timesheet/{id}", app.deleteTimesheetHandler)
+	})
+
 	return router
 }
